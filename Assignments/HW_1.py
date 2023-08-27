5c8cf8337fecfea14c695525865db15ecd4e3d72
Trudi Qi <agivenchy@hotmail.com>
2023-08-26 23:29:28 -0700

    Initial commit

---
+++ Assignments/HW_1.py
@@ -0,0 +1,21 @@
+## NOTE: This file is just for code, 
+## write your written report in a separate file and upload it as PDF
+
+
+#TODO 1. In the lines below,
+#   Output Hello, what is your name?
+#   Ask the user for their name
+#   Print out Hello [their name]
+
+
+
+
+
+#TODO 2. Can you figure out what's causing these syntax errors and solve them?
+# uncomment each one and run to see the error message
+
+##-------------------------------------------------------
+# favorite_class = input("What's your favorite class? "))
+
+##-------------------------------------------------------
+# test = 'My grandma's pie is my favorite pie'
---
+++ Lectures/IntroToCS_1_BLANK.py
@@ -0,0 +1,21 @@
+## NOTE Use # to add a comment or remove a line of code from running
+#  Uncomment a line of code by remoing # in front of it
+
+
+# print output, e.g., Welcome
+print("Welcome")
+
+# user input
+print("What is this class?")
+
+
+# SyntaxError
+
+
+# TypeError
+
+
+# NameError
+
+
+# ValueError
