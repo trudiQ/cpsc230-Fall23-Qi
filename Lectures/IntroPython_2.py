4218f2fe66709a8124b9e010a6db85d82de853ba
Trudi Qi <agivenchy@hotmail.com>
2023-09-06 16:04:11 -0700

    added solution to introPython_2.py

---
+++ Lectures/IntroPython_2.py
@@ -0,0 +1,28 @@
+# Class Activities (Lecture 2)
+# Rename the file name to "IntroPython_2_YOURNAME.py" and Submit
+
+'''
+1: Create a variable called number_of_units and assign 3 to it
+    Then in the next line, print out number_of_units
+'''
+number_of_units = 3
+print(number_of_units)
+
+
+'''
+2: What's wrong with the following lines of code? can you fix it?
+print out the student's name by using its variable
+'''
+Student_2 = "Jane" # uncomment the code to see what's happened?
+
+
+
+'''
+3. The code below calculates the area of a triangle. Add comments to 
+each line explaining what it is doing.
+'''
+base = 10 # create a variable called base and then assign 10 to it
+height = 3 # create a variable called height and then assign 3 to it
+area = base * height # create a variable called area and assign the multiplication of values of base and height to it
+area = area/2 # divide the value of variable area by 2 and then assign the result back to variable area
+print("The triangle area is", area) # print out the value of area and put it in a sentence
