dd69701aabd2be51d43f6e21fcc1b09bc3d03e95
Trudi Qi <agivenchy@hotmail.com>
2023-09-20 16:10:23 -0700

    added HW_3

---
+++ Assignments/HW_3.py
@@ -0,0 +1,43 @@
+# Homework for Lectures 5
+# Rename the file as HW_3_YOURNAME.py
+
+
+'''
+1. Write code that checks if the input number from the user is positive, negative, or zero.
+Print the result in this format: "-5 is negative." where -5 is user input.
+Hint: convert the user input into an integer before doing conditionals. 
+Use if-elif-else statements. 
+Hint: Refer to the example "The elif Statement" of Page 8 in Lecture 6.
+'''
+
+
+
+
+'''
+2. Write code to determine if a person is eligible for a discount based on age 
+and membership status. 
+There are two scenarios where a person is eligible:
+    The person is 60 years or older, 
+    or, the person is 50 years or older and having a membership.
+    Print out "You are eligible for a discount." 
+Otherwise, print out "You are not eligible for a discount."
+Use if-else and logical operators for this task.
+Hint: Refer to the example in "Combining Multiple Conditions" of Page 17 in Lecture 6.
+'''
+age = 65 
+is_memeber = True
+
+# Write your code below
+
+
+
+
+
+'''
+3. Can you write a COMPLETE game of "Rock, Paper, Scissors" between two players, and determine who wins the game.
+Ask the user for TWO inputs for the players and store them in the variables player1_choice and player2_choice.
+Comparing those two variables using if-elif-else statements. 
+Print who wins the game and how in one print statement.
+For example, "Player 1 wins! Rock crushes Scissors!" (assume player1 chooses Rock and player 2 chooses Scissors)
+If there is a tie, then print "It's a tie. One more time!"
+'''
