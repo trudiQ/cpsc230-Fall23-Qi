ba4b530c3f1a323d3d3a5d637b397551f19feaf6
Trudi Qi <agivenchy@hotmail.com>
2023-09-24 18:44:42 -0700

    added lecture 7

---
+++ Lectures/WhileLoop_7_BLANK.py
@@ -0,0 +1,56 @@
+# Class Activities (Lecture 7)
+
+'''
+0. What's wrong with this while loop and how can you fix it?
+'''
+#x = 1
+#while x < 10:
+#     print(x)
+
+
+'''
+1. Using a while loop, print out all the numbers between 1 and 10 (including).
+'''
+
+
+
+
+
+
+'''
+2. Using a while loop, print out all the numbers between 1 and 100 (including) that are
+divisible by both 5 and 9.
+Add comment for each line to explain the code
+'''
+
+
+
+
+    
+
+'''
+3. Using a while loop, print out all the numbers between 1 and 100 (including) that are
+divisible by both 5 and 9.
+Exit the loop when the number is greater than 50.
+Using break for this question.
+Add comment for each line to explain the code
+'''
+
+
+
+
+
+
+
+'''
+4. Using a while loop, print out all the numbers between 1 and 10 (including) 
+except for those which are divisiable by 3.
+Using continue for this question.
+Add comment for each line to explain the code
+'''
+
+
+
+
+
+
