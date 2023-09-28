aedab493877d0aae902b23a8da6e6c05f61836fc
Trudi Qi <agivenchy@hotmail.com>
2023-09-27 23:26:40 -0700

    added HW4

---
+++ Assignments/HW_4.py
@@ -0,0 +1,33 @@
+# Homework for Lecture 7
+# Rename the file as HW_4_YOURNAME.py
+
+'''
+1. Write a while loop that repeats as long as user_input >= 1. In each loop iteration, 
+frist divide user_input by 2, then print user_input.
+'''
+user_input = int(input("Input a number"))
+# write your code below
+
+
+
+
+'''
+2. Write a while loop that only prints out the odd integers from 1 to 11 (inclusive), 
+i.e., you should see, 1, 3, 5, 7, 9, 11.
+'''
+
+
+
+'''
+3. Write a program that asks the user to guess your age. If they
+guess too high, print out "wow I am younger than that!". If they guess
+too low, print out "hahaha I am older than that!". Once they
+guess correctly, print out a message telling them they are right and exit the loop.
+Use if-elif-else statements for this.
+'''
+# Uncomment the code below and COMPLETE it.
+#my_age = 18
+#while True:
+#    user_input = int(input("Guess my age!"))
+
+
