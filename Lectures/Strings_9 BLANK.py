c626540d2049ef2c82a69f4f925d3871ad953b82
Trudi Qi <agivenchy@hotmail.com>
2023-10-03 21:36:37 -0700

    added lecture 9

---
+++ Lectures/Strings_9 BLANK.py
@@ -0,0 +1,34 @@
+# Class Activities (Lecture 9)
+
+'''
+1. Ask the user to input a random word, assign it to my_string.
+First, check if my_string is all uppercased. 
+If yes, print out "Good, all're uppercased!".
+Otherwise, convert my_string to all uppercase and print out the result.
+'''
+
+
+
+
+'''
+2. Replace all occurrences of "coffee" with "hot chocolate" and "programmer" 
+with "coder". Print out the result.
+'''
+paragraph = """In the town of Coderville, every programmer loved to drink coffee; 
+coffee gave them the energy to code late into the night. 
+However, some programmers preferred tea over coffee."""
+
+
+
+
+'''
+3. Ask the user to type at least two words separated spaces, store the input to a variable named my_string.
+    a. Check if the first character is capitalized, print out "[user input]'s first character is capitalized".
+    b. Check if the first character is a number, , print out "[user input]'s first character is a number".
+    c. Print how many words in my_string
+        Hint: Split my_strings by space and print the length of the list of tokens.
+'''
+
+
+
+
