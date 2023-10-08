7af83db9586340056b95189dfb831c0c5102ccca
Trudi Qi <agivenchy@hotmail.com>
2023-10-08 16:15:51 -0700

    added in-class 10

---
+++ Lectures/ForLoop_10_BLANK.py
@@ -0,0 +1,59 @@
+# Class Activities (Lecture 10)
+
+'''
+1. Use a for loop and range() to create integers from 0 to 5 and print them out.
+'''
+print("QUESTION 1:")
+# write your code below
+
+
+
+
+'''
+2. Use a for loop and range() to create integers from 0 to 5 and print them out.
+Use a loop variable to count iteration number and print it out after the for loop.
+'''
+print("QUESTION 2:")
+# write your code below
+
+
+
+
+'''
+3. Put the names of your groupmates in a list (example below), then use a for
+loop to print out "You're a valuable member of group [group number], [Name]"
+for each name.
+'''
+print("QUESTION 3:")
+# write your code below
+names = ['Bill', 'Nicole', "John"] # replace with your groupmates names in the list
+
+
+
+'''
+4. Use a for loop to iterate a list of temperatures (Celsius), use variable num_neg 
+    to count the number of below-freezing (< 0) temperatures, and print the result.
+'''
+print("QUESTION 4:")
+temperatures = [30, 20, 2, -5, -15, -8, -1, 0, 5, 35]
+num_neg = 0
+# write your code below
+
+
+
+
+
+'''
+5. Ask the user to input two words of the SAME LENGTH. Using a for loop, count
+how many corresponding letters are the same in the two strings. 
+For example in spam and span, 3 of the letters (s,p,a) are the same. 
+In the words bitter and better,5 of the letters are the same. 
+Print out a message telling the user how many letters are the same.
+Hint: Iterate each pair of characters of the same index in both strings and compare.
+'''
+print("QUESTION 5: ")
+# write your code below
+
+
+
+
