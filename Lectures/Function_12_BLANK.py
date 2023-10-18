14d558ff1956cc0f1026608cfd5836f9901b9a70
Trudi Qi <agivenchy@hotmail.com>
2023-10-17 22:10:41 -0700

    added in-class 12

---
+++ Lectures/Function_12_BLANK.py
@@ -0,0 +1,53 @@
+# Class Activities (Lecture 12)
+import random
+
+'''
+0. randint(a, b) is a function from a built-in library named random (see above import random)
+that can be used to generate a random integer, N, between two integers, a and b, where a <= N <= b.
+PART 1: Define a function, random_sum, that generates two random numbers between a and b (a < b) and return their sum.
+PART 2: Invoke (use) the function to calculate the sum of two random numbers between 1 and 100 (inclusive)
+and print the result.
+'''
+print("QUESTION 0 ---------------")
+# example: generate a random number between 0 and 9 (inclusive)
+x = random.randint(0, 9)
+print(x)
+
+# write your code below
+
+
+
+'''
+1. Ask the user for their name, and print a welcome message to the user, e.g., "Welcome, Nicole!"
+    PARK 1: Define a function that prints a welcome message using their input name.
+    PARK 2: Invoke (use) the function and input the name user inputs to the function.
+'''
+print("QUESTION 1 ---------------")
+# write your code below
+
+
+
+'''
+2. PART 1: Define a function that calculate a rectangle's perimeter based on
+   its length and width as input. return its perimeter as output.
+   Hint: rectangle's perimeter is 2 * (length + width). 
+   PART 2: Invoke (use) the function your defined to calculate the perimeter 
+   of a rectangle of length 5 and width 3 and print the result.
+'''
+print("QUESTION 2 ---------------")
+# write your code below
+
+
+
+'''
+3. PART 1: Define a function returns True if the input number is greater than 10,
+    and returns False otherwise.
+   Part 2: Use the function to check the input argument of 16 and print out:
+   "[argument] is greater than 10" or "[argument] is not greater than 10"
+'''
+print("QUESTION 3 ---------------")
+# write your code below
+
+
+
+
