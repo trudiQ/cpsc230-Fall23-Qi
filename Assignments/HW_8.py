48e23f348b32caf3ab469441cacd36b95fc1f7ff
Trudi Qi <agivenchy@hotmail.com>
2023-10-25 16:23:15 -0700

    added HW_8

---
+++ Assignments/HW_8.py
@@ -0,0 +1,51 @@
+# Homework for Lectures 13 & 14
+# Rename the file as HW_8_YOURNAME.py
+
+
+'''
+1. 
+a) Write a function, euclidean(), that takes in 4 parameters, x1, y1, x2, and y2,
+    and calculate the euclidean distance between two points (x1, y1) and (x2, y2).
+    Return the euclidean distance as a number.
+    Make the default arguments 1 for all four parameters.
+b) Calcuate and print the euclidean distance between two points (1, 1) and (2, 2) by calling the function.
+    (you should see 1.4142.. which is the square root of 2)
+NOTE: FULL CREDIT: Use exponent operator ** and other necessary arithmetic operators for this question.
+
+Hint:
+    Euclidean distance = square root of (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)
+    Refer to the power function in Q3 in the in-class exercise for lecture 13 on
+    how to calculate square root.
+'''
+print("QUESTION 1: ")
+# write your code below
+
+
+
+
+
+'''
+2. There is a triangle of three vertices (points) (0, 0), (2, 2), (4, 0). 
+    a) Create a list of tuples, named coordinates, to store those three vertices. 
+    b) Use a FOR-LOOP to iteratue the list and print all the vertex coordinates 
+        in the format of "X: 2, Y: 2".
+    c) Calculate the euclidean distances (using Q1's function) between the first two vertices (tuples) in
+    coordinates, Print out the result in this format: 
+        "The euclidean distance between [first vertex] and [second vertex] is [result]"
+    NOTE: Use indexing to access the first two tupes in coordinates; DO NOT use those numbers directly.
+Hint: 
+    Refer to Page 9 in Lecture 14 on how to access an item in a tuple in a list.  
+'''
+print("QUESTION 2: ")
+# write your code below
+
+
+
+
+
+
+
+
+
+
+
