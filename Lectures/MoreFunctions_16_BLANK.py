4c489384dd23eaef985efec2979dda645eb1b99d
Trudi Qi <agivenchy@hotmail.com>
2023-10-31 21:11:25 -0700

    added in-class 16

---
+++ Lectures/MoreFunctions_16_BLANK.py
@@ -0,0 +1,31 @@
+# Class Activities (Lecture 16)
+
+'''
+1. Simulate a simple banking system with functions to deposit, withdraw, and check balance.
+    a) Create a global variable account_balance initialized to 0.
+    b) Write a function, named deposit(amount), that adds the input amount to account_balance.
+    c) Write a function, named withdraw(), that only subtracts $10 from account_balance each time. 
+    e) Write a function, named check_balance(), that prints the current account_balance.
+    f) Perform a series of deposits and withdrawals, and check the balance as below. Print out the result.
+        Deposite $100 => Withdraw $10 => Withdraw $10 => Deposite $30 => Check_balance
+NOTE: Define all the required functions first and then call them one after another based on f)
+'''
+print("QUESTION 1: ")
+# write your code below
+
+
+
+
+
+
+'''
+2. Write a function, top_3_movies(), that asks the user to list their top 3 favorite movies 
+separated by ','. Split the string into a list of the 3 movie titles (sub-strings).
+Keep asking the user if they do not provide three movies titles.
+Return those three movies titles.
+
+Call your function top_3_movies(), and store those 3 movie titles using 3 variables, 
+mov1, mov2, and mov3. Print out those 3 movie titles. 
+'''
+print("QUESTION 2: ")
+# write your code below
